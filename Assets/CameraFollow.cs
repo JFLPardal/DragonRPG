cfaeb1138a16b827ab87904a8fa8c97d83136142
JFLPardal <jflpardal@gmail.com>
2017-12-23 23:27:10 +0000

    section 1 lecture 8
    prototype with placeholders
    first modulation of the level

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	Transform target;
+
+
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void LateUpdate () {
+		transform.position =  target.position;
+	}
+}
