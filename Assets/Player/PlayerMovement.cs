33e04fd4250a9bea896a14a9adc3b6983e5768b1
JFLPardal <jflpardal@gmail.com>
2017-12-26 04:23:43 +0000

    section 2 lecture 33
    * gizmos
    * health bars

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -7,10 +7,11 @@ using UnityStandardAssets.Characters.ThirdPerson;
 public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField] float walkMoveStopRadius = 0.2f;
+	[SerializeField] float attackMoveStopRadius = 5f;
 
 	ThirdPersonCharacter thirdPersonCharacter;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+    Vector3 currentDestination, clickPoint;
      
 	bool isInControllerMode = false;	//TODO consider make this variable static
 
@@ -18,16 +19,15 @@ public class PlayerMovement : MonoBehaviour
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
         thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
-        currentClickTarget = transform.position;	
+        currentDestination = transform.position;	
     }
 
     private void FixedUpdate()
-    {
-		if (Input.GetKeyDown (KeyCode.G)) //G for gamepad TODO allow the player to choose this button
+	{//TODO allow the player to choose this button
+		if (Input.GetKeyDown (KeyCode.G)) //G for gamepad 
 		{ 
 			isInControllerMode = !isInControllerMode;
-			currentClickTarget = transform.position;	//clear click target
-			print("switch");
+			currentDestination = transform.position;	//clear click target
 		}
 		if (isInControllerMode)
 			ProcessGamepadMovement ();
@@ -52,25 +52,48 @@ public class PlayerMovement : MonoBehaviour
 	{
 		if (Input.GetMouseButton (0)) 
 		{
+			clickPoint = cameraRaycaster.hit.point;
 			switch (cameraRaycaster.currentLayerHit) {
 			case Layer.walkable:
-				currentClickTarget = cameraRaycaster.hit.point;
-				var playerToClickPoint = currentClickTarget - transform.position;
-				if (playerToClickPoint.magnitude > walkMoveStopRadius)
-					thirdPersonCharacter.Move (playerToClickPoint, false, false);
-				else
-					thirdPersonCharacter.Move (Vector3.zero, false, false);
+				currentDestination = ShortDestination(clickPoint, walkMoveStopRadius);
 				break;
 			case Layer.enemy:
-				print ("not walking to enemy");
+				currentDestination = ShortDestination (clickPoint, attackMoveStopRadius);
 				break;
 			default:
 				print ("unexpected layer");
 				return;
 			}
 		}
+		WalkToDestination ();
+	}
+
+	private void WalkToDestination ()
+	{
+		var playerToClickPoint = currentDestination - transform.position;
+		if (playerToClickPoint.magnitude > 0)
+			thirdPersonCharacter.Move (playerToClickPoint, false, false);
 		else
 			thirdPersonCharacter.Move (Vector3.zero, false, false);
+	}
+
+	private Vector3 ShortDestination(Vector3 destination, float shortening)
+	{
+		Vector3 reductionVector = (destination - transform.position).normalized * shortening;
+		return destination - reductionVector;
+	}
+
+	void OnDrawGizmos()
+	{
+		//draw movement gizmos
+		Gizmos.color = Color.black;
+		Gizmos.DrawLine (transform.position, currentDestination);
+		Gizmos.DrawSphere (currentDestination, 0.1f);
+		Gizmos.DrawSphere (clickPoint, 0.1f);
+
+		//draw attack gizmos
+		Gizmos.color = new Color(255, 0, 0, 0.5f);
+		Gizmos.DrawWireSphere (transform.position, attackMoveStopRadius);
 	}
 }
 
